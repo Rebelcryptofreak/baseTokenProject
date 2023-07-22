735c10ec88b2856d1caa5bd47e100e76de0400d1
rebel <139927829+Rebelcryptofreak@users.noreply.github.com>
2023-07-23 00:38:48 +0330

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0xb295490fb15e4236d1ae08ff2412e722c7f28cdf";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
